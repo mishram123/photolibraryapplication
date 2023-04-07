fe2e6db20add038c22e2a2fbc4c0f4cf630acbff
Mannan Mishra <mm3033@scarletmail.rutgers.edu>
2023-04-07 12:17:05 -0400

    logout and quit work for user subsystem

--- src/PhotoAlbum/UserSystemController.java
+++ src/PhotoAlbum/UserSystemController.java
@@ -63,5 +63,36 @@ public class UserSystemController {
       });
   }
 
+  @FXML
+  private void logout(ActionEvent event){
+    Dialog<ButtonType> dialog = new Dialog<>();
+    dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("LogoutDialog.fxml"));
+    try {
+        dialog.getDialogPane().setContent(loader.load());
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    dialog.getDialogPane().getButtonTypes().addAll(ButtonType.YES, ButtonType.CANCEL);
+    dialog.showAndWait().ifPresent(response -> {
+        if(response == ButtonType.YES){
+            FXMLLoader nextloader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Parent secondPage = null;
+            try {
+               secondPage = nextloader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Get the current scene
+            Scene currentScene = ((Node) event.getSource()).getScene();
+
+            // Set the new root node to the current scene
+            currentScene.setRoot(secondPage);
+        }
+    });
+
+  }
+
 
 }
