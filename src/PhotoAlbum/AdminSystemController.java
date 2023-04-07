cc8d32b689c1cc3585dea0b57bfab2cf0870e750
Mannan Mishra <mm3033@scarletmail.rutgers.edu>
2023-04-07 13:39:08 -0400

    started creating createUser and Tableview in Admin System

--- src/PhotoAlbum/AdminSystemController.java
+++ src/PhotoAlbum/AdminSystemController.java
@@ -1,24 +1,26 @@
 package PhotoAlbum;
+
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.application.Platform;
 import java.io.IOException;
-
-import javax.swing.text.DefaultStyledDocument.ElementSpec;
 
 import javafx.application.Platform;
 
 
 public class AdminSystemController {
   @FXML
-  private TextField usernameField;
+  private TextField usernameField = new TextField();
   @FXML
   private Label titleLabel;
   @FXML
@@ -30,8 +32,43 @@ public class AdminSystemController {
   @FXML
   private Button logoutButton;
   @FXML
-  private TableView<String> usernameListTable;
+  private TableView<String> usernameListTable = new TableView<>();
 
+  @FXML
+  ObservableList<String> userNameData = FXCollections.observableArrayList();
+
+  TableColumn<String, String> usernameListColumn = new TableColumn<>("Username List");
+
+  @FXML
+  public void initialize() {
+    usernameListColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+    usernameListColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+    usernameListTable.setItems(userNameData);
+    usernameListTable.getColumns().add(usernameListColumn);
+    
+  }
+
+    @FXML
+    private void createUser(ActionEvent event) throws IOException{
+        String enteredUser = usernameField.getText();
+
+        boolean userExists = usernameListTable.getItems().stream().anyMatch(item -> item.compareTo(enteredUser)==0);
+
+        if(userExists){
+            Dialog<ButtonType> dialog = new Dialog<>();
+            dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("userExists.fxml"));
+            dialog.getDialogPane().setContent(loader.load());
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+            Button closeButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.CLOSE);
+            closeButton.setOnAction(e -> dialog.close());
+            dialog.showAndWait();
+        }else{
+            userNameData.add(enteredUser);
+            usernameField.clear();
+            usernameListTable.refresh();
+        }
+    }
     @FXML
     private void quit(ActionEvent event) throws IOException{
         Dialog<ButtonType> dialog = new Dialog<>();
@@ -45,6 +82,36 @@ public class AdminSystemController {
                 Platform.exit();
             }
         });
+    }
+    @FXML
+    private void logout(ActionEvent event){
+      Dialog<ButtonType> dialog = new Dialog<>();
+      dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("LogoutDialog.fxml"));
+      try {
+          dialog.getDialogPane().setContent(loader.load());
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      dialog.getDialogPane().getButtonTypes().addAll(ButtonType.YES, ButtonType.CANCEL);
+      dialog.showAndWait().ifPresent(response -> {
+          if(response == ButtonType.YES){
+              FXMLLoader nextloader = new FXMLLoader(getClass().getResource("Login.fxml"));
+              Parent secondPage = null;
+              try {
+                 secondPage = nextloader.load();
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+  
+              // Get the current scene
+              Scene currentScene = ((Node) event.getSource()).getScene();
+  
+              // Set the new root node to the current scene
+              currentScene.setRoot(secondPage);
+          }
+      });
+  
     }
 
 }
