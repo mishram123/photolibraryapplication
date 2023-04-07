3220e6b085950ad90fd1f53ae0d410d5f8f1863a
Soban Chaudhry <sac399@scarletmail.rutgers.edu>
2023-04-07 15:14:18 -0400

    the album class has been added

---
+++ src/PhotoAlbum/Album.java
@@ -0,0 +1,42 @@
+package PhotoAlbum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    private String name;
+    private List<Photo> photos;
+
+    public Album(String name) {
+        this.name = name;
+        this.photos = new ArrayList<>();
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public List<Photo> getPhotos () {
+        return photos;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    //add a photo
+    public void addPhoto(Photo photo) {
+        photos.add(photo);
+    }
+
+    //remove a photo
+    public void deletePhoto(Photo photo) {
+        photos.remove(photo);
+    }
+}
