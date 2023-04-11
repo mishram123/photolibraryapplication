82b7d05504b6b1287c42c9f8a84ffc9f7ebd6d3d
Soban Chaudhry <sac399@scarletmail.rutgers.edu>
2023-04-11 15:48:20 -0400

    add photos feature improved

--- src/PhotoAlbum/albumDisplayController.java
+++ src/PhotoAlbum/albumDisplayController.java
@@ -11,8 +11,29 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import java.time.LocalDateTime;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import java.io.IOException;
+import javafx.collections.*;
+import javafx.application.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.scene.layout.AnchorPane;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,11 +48,16 @@ public class albumDisplayController {
     @FXML
     private Button addPhotoButton;
 
-    private Album currentAlbum;
+    private static Album currentAlbum;
     private User currentUser;
 
+    public void initialize() {
+        currentAlbum = UserSystemController.getCurAlbum();
+        displayPhotoPreviews();
+      }
+
     public void setCurrentAlbum(Album album) {
-        this.currentAlbum = album;
+        currentAlbum = album;
         albumNameLabel.setText(album.getName());
         displayPhotoPreviews();
     }
@@ -78,4 +104,50 @@ public class albumDisplayController {
         Scene currentScene = ((Node) event.getSource()).getScene();
         currentScene.setRoot(userSystemPage);
     }
+
+    @FXML
+  private void quit(ActionEvent event) throws IOException{
+      Dialog<ButtonType> dialog = new Dialog<>();
+      dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("QuitDialog.fxml"));
+      dialog.getDialogPane().setContent(loader.load());
+      dialog.getDialogPane().getButtonTypes().addAll(ButtonType.YES, ButtonType.CANCEL);
+      dialog.showAndWait().ifPresent(response -> {
+          if (response == ButtonType.YES) {
+              // If the user clicked YES, exit the program
+              Platform.exit();
+          }
+      });
+  }
+
+  @FXML
+  private void logout(ActionEvent event){
+    Dialog<ButtonType> dialog = new Dialog<>();
+    dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("LogoutDialog.fxml"));
+    try {
+        dialog.getDialogPane().setContent(loader.load());
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    dialog.getDialogPane().getButtonTypes().addAll(ButtonType.YES, ButtonType.CANCEL);
+    dialog.showAndWait().ifPresent(response -> {
+        if(response == ButtonType.YES){
+            FXMLLoader nextloader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Parent secondPage = null;
+            try {
+               secondPage = nextloader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Get the current scene
+            Scene currentScene = ((Node) event.getSource()).getScene();
+
+            // Set the new root node to the current scene
+            currentScene.setRoot(secondPage);
+        }
+    });
+
+  }
 }
